a39ad2637a0d2679a0927a449bc42762fa360101
VanJackal <v@njackal.com>
2021-04-22 09:18:06 -0500

    added scaffold for web api

--- server.py
+++ server.py
@@ -5,4 +5,36 @@ from flask import Flask, request
 with open("config.yaml") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
+app = Flask(__name__)
 
+@app.route('/api/config/<serverID>', methods=['GET','PUT'])
+def remoteConfig(serverID):
+    if request.method == 'GET':
+        pass
+    elif request.method == 'PUT':
+        pass
+    return '<p>not yet implemented</p>'
+
+@app.route('/api/config', methods=['GET','PUT'])
+def localConfig():
+    if request.method == 'GET':
+        pass
+    elif request.method == 'PUT':
+        pass
+    return '<p>not yet implemented</p>'
+
+@app.route('/api/config/servers',methods=['GET', 'POST'])
+def configServers():
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        pass
+    return '<p>not yet implemented</p>'
+
+@app.route('/api/backups/start', methods=['POST'])
+def startBackup():
+    return '<p>not yet implemented</p>'
+
+@app.route('/api/backups/purge', methods=['POST'])
+def purgeBackup():
+    return '<p>not yet implemented</p>'
